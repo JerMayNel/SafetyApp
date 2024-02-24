944f5fd7243dec607898a087cab62ce27eab24fb
Nel <132349160+JerMayNel@users.noreply.github.com>
2024-02-24 21:45:17 +0800

    Additional Files

--- frontend/ButtonScreen.py
+++ frontend/ButtonScreen.py
@@ -6,10 +6,10 @@ from kivymd.app import MDApp
 class SafetyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Cyan" # Fix
-        self.theme_cls.primary_hue = "600"  # Corresponding to the RGB components, since Cyan is a shade of Teal.
+        self.theme_cls.primary_palette = "Cyan"  # Fix
+        self.theme_cls.primary_hue = "500"  # Corresponding to the RGB components, since Cyan is a shade of Teal.
         self.theme_cls.accent_hue = "400"  # Adjust as needed, based on your color preferences.
-        return Builder.load_file('bbar.kv')
+        return Builder.load_file('mainscreen.kv')
 
 
 SafetyApp().run()
---
+++ frontend/LogInScreen.py
@@ -0,0 +1,3 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
