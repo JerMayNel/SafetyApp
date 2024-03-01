65c6e90444f1ae98ad9078959c57ae2feb7a9f95
Nel <132349160+JerMayNel@users.noreply.github.com>
2024-03-01 14:55:27 +0800

    Add AddContactsScreen
    The gui will now swtich to AddContact if the add button was pressed.

--- frontend/ButtonScreen.py
+++ frontend/ButtonScreen.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivy.garden.mapview import MapMarker, MapSource
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
 class SafetyApp(MDApp):
@@ -10,6 +11,19 @@ class SafetyApp(MDApp):
         self.theme_cls.primary_hue = "500"  # Corresponding to the RGB components, since Cyan is a shade of Teal.
         self.theme_cls.accent_hue = "400"  # Adjust as needed, based on your color preferences.
         return Builder.load_file('mainscreen.kv')
+
+
+class MainScreen(Screen):
+    pass
+
+
+class AddContactsScreen(Screen):
+    pass
+
+
+screen_manager = ScreenManager()
+screen_manager.add_widget(MainScreen(name='Main'))
+screen_manager.add_widget(AddContactsScreen(name='AddContacts'))
 
 
 SafetyApp().run()
