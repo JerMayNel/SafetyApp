d232d537af5aba447b35fb88a185313ecd649cf4
Nel <132349160+JerMayNel@users.noreply.github.com>
2024-03-01 22:49:01 +0800

    Creating a get_data function
    The get_data function will now take datas from the text fields on the AddContactsScreen and print it on the console for now and some adjustments.

--- frontend/ButtonScreen.py
+++ frontend/ButtonScreen.py
@@ -2,6 +2,9 @@ from kivy.lang import Builder
 from kivy.garden.mapview import MapMarker, MapSource
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.core.window import Window
+
+Window.size = (400, 600)  # For phone size development only
 
 
 class SafetyApp(MDApp):
@@ -18,12 +21,28 @@ class MainScreen(Screen):
 
 
 class AddContactsScreen(Screen):
-    pass
+    """ Sample getting data for now, printing datas on the screen.
+     IMPORTANT: for backend soon!
+     Create a backend code to create a card / gui for new contacts, based on the database inputted by
+     text fields, ensure that the cards has a delete option to delete cards.
+    """
+    def get_data(self):
+        phone_number_text = self.ids.phone_number.text
+        name_text = self.ids.name.text
+        email_text = self.ids.email.text
+
+        print("Phone Number:", phone_number_text)
+        print("Name:", name_text)
+        print("Email:", email_text)
+
+        self.manager.current = 'Main'
+        self.ids.phone_number.text = ""
+        self.ids.name.text = ""
+        self.ids.email.text = ""
 
 
 screen_manager = ScreenManager()
 screen_manager.add_widget(MainScreen(name='Main'))
 screen_manager.add_widget(AddContactsScreen(name='AddContacts'))
-
 
 SafetyApp().run()
