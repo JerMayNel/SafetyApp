358c87e31a5f3acf6b1fed1dd66a12089acd1c23
Nel <132349160+JerMayNel@users.noreply.github.com>
2024-03-02 18:14:42 +0800

    Updated Profile Buttons
    Created some screen for buttons. The buttons is working now with switching screens when pressed. Also, TODO was placed for future plans.

--- frontend/ButtonScreen.py
+++ frontend/ButtonScreen.py
@@ -3,17 +3,44 @@ from kivy.garden.mapview import MapMarker, MapSource
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 
 Window.size = (400, 600)  # For phone size development only
 
 
 class SafetyApp(MDApp):
+    dialog = None
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Cyan"  # Fix
+        self.theme_cls.primary_palette = "Cyan"  # TODO: Fix DESIGN
         self.theme_cls.primary_hue = "500"  # Corresponding to the RGB components, since Cyan is a shade of Teal.
         self.theme_cls.accent_hue = "400"  # Adjust as needed, based on your color preferences.
         return Builder.load_file('mainscreen.kv')
+
+    def logout_alert(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Log Out",
+                text="Are you sure you want to log-out?",
+                buttons=[
+                    MDFlatButton(
+                        text="No",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color, on_release=self.close_dialog
+                    ),
+                    MDFlatButton(
+                        text="Yes",
+                        theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,  # TODO: Implement log-out function
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
 
 
 class MainScreen(Screen):
@@ -21,11 +48,12 @@ class MainScreen(Screen):
 
 
 class AddContactsScreen(Screen):
-    """ Sample getting data for now, printing datas on the screen.
-     IMPORTANT: for backend soon!
-     Create a backend code to create a card / gui for new contacts, based on the database inputted by
-     text fields, ensure that the cards has a delete option to delete cards.
+    """ TODO: Sample getting data for now, printing datas on the screen.
+     TODO: IMPORTANT: for backend soon!
+     TODO: Create a backend code to create a card / gui for new contacts, based on the database inputted by
+     TODO: -text fields, ensure that the cards has a delete option to delete cards.
     """
+
     def get_data(self):
         phone_number_text = self.ids.phone_number.text
         name_text = self.ids.name.text
@@ -41,8 +69,18 @@ class AddContactsScreen(Screen):
         self.ids.email.text = ""
 
 
+class EditProfileScreen(Screen):
+    pass
+
+
+class AboutUsScreen(Screen):
+    pass
+
+
 screen_manager = ScreenManager()
 screen_manager.add_widget(MainScreen(name='Main'))
 screen_manager.add_widget(AddContactsScreen(name='AddContacts'))
+screen_manager.add_widget(EditProfileScreen(name='EditProfile'))
+screen_manager.add_widget(AboutUsScreen(name='AboutUs'))
 
 SafetyApp().run()
