0b5eebe704b6b72c8a509c705c029c46aa7a31a9
Nel <132349160+JerMayNel@users.noreply.github.com>
2024-03-10 17:08:13 +0800

    Sign / Log In Screen Huge Update
    Log In has now working buttons like Forgot Password and Sign Up. Sign up has now more screen like AddTrustedContacts and AskLocationScreen. Forgot Password Screen is also implemented. TODO's also placed.

--- frontend/LogInScreen.py
+++ frontend/LogInScreen.py
@@ -1,9 +1,25 @@
+# import pyrebase
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 Window.size = (300, 600)
+
+"""
+    firebaseConfig = {
+    'apiKey': "AIzaSyDF5SAD9euJbbsUEz3yQY6W3LgvA-0bM8U",
+    'authDomain': "finalproject-8b6b4.firebaseapp.com",
+    'projectId': "finalproject-8b6b4",
+    'databaseURL': "https://finalproject-8b6b4-default-rtdb.firebaseio.com",
+    'storageBucket': "finalproject-8b6b4.appspot.com",
+    'messagingSenderId': "29231288442",
+    'appId': "1:29231288442:web:03f8123760578266e2abc3",
+    'measurementId': "G-SR5YY8FEGW"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+"""
 
 
 class LoginScreen(MDApp):
@@ -20,12 +36,38 @@ class LogInScreen(Screen):
 
 
 class SignUpScreen(Screen):
+    """
+    def create_account(self, email, password):
+        emailpy = email
+        passwordpy = password
+        auth.create_user_with_email_and_password(emailpy, passwordpy)
+    """
+    pass
+
+
+class AddTrustedContactsScreen(Screen):
+    pass
+
+
+class AskLocationScreen(Screen):
+    pass
+
+
+class ForgotPasswordScreen(Screen):
+    pass
+
+
+class PinScreen(Screen):
     pass
 
 
 screen_manager = ScreenManager()
 screen_manager.add_widget(LogInScreen(name='LogIn'))
 screen_manager.add_widget(SignUpScreen(name='SignUp'))
+screen_manager.add_widget(AddTrustedContactsScreen(name='AddTrustedContacts'))
+screen_manager.add_widget(AskLocationScreen(name='AskLocation'))
+screen_manager.add_widget(ForgotPasswordScreen(name='ForgotPassword'))
+screen_manager.add_widget(PinScreen(name='Pin'))
 
 if __name__ == "__main__":
     LoginScreen().run()
